bfb4b1cb778f7d836de28a8c84517247a848b2b0
Leonardo Galbiere Arruda <leogalbierearruda@gmail.com>
2021-10-18 23:04:01 -0300

    Github routes -> login and callback

--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,7 +1,20 @@
-import express from "express"
+import 'dotenv/config'
+import express from 'express'
 
 const app = express()
 
+app.get('/github', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  )
+})
+
+app.get('/signin/callback', (req, res) => {
+  const { code } = req.query
+
+  return res.json(code)
+})
+
 app.listen(4000, () => {
-  console.log("🚀 Server is running on port 4000")
+  console.log('🚀 Server is running on port 4000')
 })
