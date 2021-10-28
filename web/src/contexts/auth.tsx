31703c95fadfffce5dd4c10a5af21798867192b8
Leonardo Galbiere Arruda <leogalbierearruda@gmail.com>
2021-10-27 21:17:21 -0300

    features login

---
+++ web/src/contexts/auth.tsx
@@ -0,0 +1,79 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { api } from '../services/api'
+
+type User = {
+  id: string
+  name: string
+  login: string
+  avatar_url: string
+}
+
+type AuthContextData = {
+  user: User | null
+  signInUrl: string
+}
+
+type AuthProvider = {
+  children: ReactNode
+}
+
+type AuthResponse = {
+  token: string
+  user: {
+    id: string
+    avatar_url: string
+    name: string
+    login: string
+  }
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null)
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c0828bab462291d262ef`
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
+
+    const { token, user } = response.data
+
+    localStorage.setItem('@doWhile:token', token)
+
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@doWhile:token')
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`
+
+      api.get<User>('profile').then((response) => {
+        setUser(response.data)
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    const url = window.location.href
+    const hasGithubCode = url.includes('?code=')
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode)
+
+      signIn(githubCode)
+    }
+  })
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
